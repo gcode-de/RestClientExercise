b04081e7be34b476eb39bdd56ab0833d7371be81
Samuel Gesang <sgesang@mailo.com>
2024-05-15 11:34:22 +0200

    Initial commit

---
+++ src/main/java/de/samuelgesang/restclientexercise/RestClientExerciseApplication.java
@@ -0,0 +1,13 @@
+package de.samuelgesang.restclientexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/samuelgesang/restclientexercise/RestClientExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samuelgesang.restclientexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
